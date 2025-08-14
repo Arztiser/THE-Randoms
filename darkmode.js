7e04607d9b57c32e9693e3526c079bc2cb2cc762
Arztiser <145240129+Arztiser@users.noreply.github.com>
2025-08-13 21:49:32 -0500

    Update darkmode.js

--- darkmode.js
+++ darkmode.js
@@ -8,7 +8,7 @@
   function isHomepage() {
     // normalize trailing slash
     const p = location.pathname.replace(/\/+$/, '');
-    return p === '' || p === '/' || p.endsWith('/test.html') || p.endsWith('/index');
+    return p === '' || p === '/' || p.endsWith('/test.html') || p.endsWith('/test');
   }
 
   /* --------- Material Icons injection ------- */
