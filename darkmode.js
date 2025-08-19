7e1916998941a8618de543cceefe5162c191f15d
Arztiser <145240129+Arztiser@users.noreply.github.com>
2025-08-19 17:31:39 -0500

    Update darkmode.js

--- darkmode.js
+++ darkmode.js
@@ -29,7 +29,7 @@
     #topnav-icons {
       display: flex;
       align-items: center;
-      margin-left: 110px; /* much closer to hamburger */
+      margin-left: 130px; /* much closer to hamburger */
     }
     .dark-toggle {
       font-family: 'Material Symbols Outlined';
